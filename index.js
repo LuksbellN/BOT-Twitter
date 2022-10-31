28877e19930a9d0be01966a250cc19a5b02dc813
Lucas <belchior1100@gmail.com>
2022-10-31 09:55:26 -0300

    Processo oficial do dia 30/10/2022

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 import Twit from "twit";
 import dotenv from "dotenv";
 import fetch from 'node-fetch';
+import nodeSchedule from 'node-schedule';
+
+
+const endData = new Date(2022, 9, 30, 23, 1)
 
 dotenv.config();
 
 // Autenticação
 const Bot = new Twit({
-    consumer_key: process.env.API_KEY,
-    consumer_secret:process.env.API_SECRET_KEY,
-    access_token:process.env.ACCESS_TOKEN,
-    access_token_secret:process.env.ACCESS_TOKEN_SECRET,
-    timeout_ms:60 * 1000
+  consumer_key: process.env.API_KEY,
+  consumer_secret:process.env.API_SECRET_KEY,
+  access_token:process.env.ACCESS_TOKEN,
+  access_token_secret:process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms:60 * 1000
 });
-
 
 // Pegar dados disponibilizados pelo tse
 const getData = async () => {
-  let url = 'https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json';
+  let url = 'https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json';
   let results = await fetch(url);
   let json = await results.json();
   if(json) {
@@ -31,15 +34,15 @@ async function BotInit() {
   
   if(info) {
     // Separar informações úteis
-    let votosValidados = info.psa;
+    let votosValidados = info.psi;
     let candidatos = info.cand;
     var tweet = {
       status: `Atualização eleições 2° turno: \n 1-${candidatos[0].nm} - ${candidatos[0].pvap}% - ${formatar(candidatos[0].vap)} votos\n 2-${candidatos[1].nm} - ${candidatos[1].pvap}% - ${formatar(candidatos[1].vap)} votos\n ${votosValidados}% dos votos validados`
     };
-
+    
     // Tweetar
     Bot.post('statuses/update', tweet, tweeted);
-
+    
     function tweeted(error, data, response) {
       if(!error) {
         console.log("Tweet realizado!");
@@ -52,9 +55,13 @@ async function BotInit() {
   }
 }
 
-// Temporizador de atualização (5 mins)
-setTimeout(BotInit, 60*5000);
-BotInit();
+const myInterval = setInterval(BotInit, 60*1000);
+
+const shutdown = nodeSchedule.scheduleJob(endData, () => {
+  clearInterval(myInterval);
+  schedule.gracefulShutdown();
+  process.exit(0);
+})
 
 function formatar(numStr) {
   let j = 0;
